3af428f4f4cefc2962e67021f4464f8d3298e826
Sergey Lebedev <limentas@gmail.com>
2020-04-04 14:30:43 +0300

    Prepared for web publishing

--- lib/src/ui/paint_widget.dart
+++ lib/src/ui/paint_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:io';
 import 'dart:ui' as dart_ui;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:draw_a_lot/src/path_part.dart';
@@ -27,25 +28,39 @@ class PaintWidgetState extends State<PaintWidget> {
   Queue<PathPart> _pathesToDraw = new Queue<PathPart>();
   Queue<PathPart> _pathesDrawn = new Queue<PathPart>();
   Queue<PathPart> _pathesToRedo = new Queue<PathPart>();
-  dart_ui.Image _cachedBuffer;
+  dart_ui.Image _cacheBuffer;
 
   void undo() {
-    if (_pathesDrawn.isEmpty) return;
-    setState(() {
+    if (_pathesToDraw.isNotEmpty) {
+      setState(() {
+        final lastChange = _pathesToDraw.removeLast();
+        lastChange.completed = true;
+        _pathesToRedo.add(lastChange);
+        if (lastChange.cached) {
+          //needs to redraw cache
+          _updateCacheBuffer(context, forceUpdateCache: true);
+        }
+      });
+    } else if (_pathesDrawn.isNotEmpty) {
       _pathesToRedo.add(_pathesDrawn.removeLast());
-    });
+      _updateCacheBuffer(context, forceUpdateCache: true);
+    }
   }
 
   void redo() {
     if (_pathesToRedo.isEmpty) return;
-    _pathesDrawn.add(_pathesToRedo.removeLast());
+    setState(() {
+      _pathesToDraw.add(_pathesToRedo.removeLast());
+    });
+    _updateCacheBuffer(context);
   }
 
   void clean() {
     _pathesToRedo = _pathesDrawn;
-    setState(() {
-      _pathesDrawn = new Queue<PathPart>();
-    });
+    _pathesToRedo.addAll(_pathesToDraw);
+    _pathesDrawn = new Queue<PathPart>();
+    _pathesToDraw.clear();
+    _updateCacheBuffer(context, forceUpdateCache: true);
   }
 
   Future<File> saveToFile(File file) {
@@ -59,18 +74,33 @@ class PaintWidgetState extends State<PaintWidget> {
     });
   }
 
-  Future<dart_ui.Image> _drawToImage(BuildContext context, double scaleFactor) {
+  Future<dart_ui.Image> _drawToImage(BuildContext context, double scaleFactor,
+      {bool redrawCache: false}) {
     var recorder = new dart_ui.PictureRecorder();
     var imageSize = MediaQuery.of(context).size * scaleFactor;
     var canvas = Canvas(recorder);
 
     canvas.scale(scaleFactor);
-    if (_cachedBuffer == null) {
+
+    if (_cacheBuffer == null || redrawCache) {
       canvas.drawRect(Rect.fromLTWH(0, 0, imageSize.width, imageSize.height),
           new Paint()..color = Colors.white);
     } else {
-      _cachedBuffer.toByteData(format: dart_ui.ImageByteFormat.rawRgba).then((value){print("use cache = ${value.buffer.asUint8List()}");});
-      canvas.drawImage(_cachedBuffer, Offset(0, 0), Paint());
+      canvas.drawImage(_cacheBuffer, Offset(0, 0), Paint());
+    }
+
+    if (redrawCache) {
+      for (var part in _pathesDrawn) {
+        canvas.drawPath(
+            part.path,
+            new Paint()
+              ..color = part.color
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = part.penWidth
+              ..strokeCap = StrokeCap.round
+              ..strokeJoin = StrokeJoin.round
+              ..isAntiAlias = true);
+      }
     }
 
     for (var part in _pathesToDraw) {
@@ -92,20 +122,21 @@ class PaintWidgetState extends State<PaintWidget> {
         .toImage(imageSize.width.toInt(), imageSize.height.toInt());
   }
 
-  void _updateCacheBuffer(BuildContext context) {
-    var imageFuture = _drawToImage(context, 1.0);
+  void _updateCacheBuffer(BuildContext context,
+      {bool forceUpdateCache: false}) {
+    if (kIsWeb) return; //There is some bug in canvas.drawImage and cache does not work properly
+
+    var imageFuture = _drawToImage(context, 1.0, redrawCache: forceUpdateCache);
 
     imageFuture.then((value) {
       setState(() {
-        _cachedBuffer = value;
+        _cacheBuffer = value;
 
         _pathesDrawn.addAll(_pathesToDraw.where((element) => element.cached));
-        _pathesToDraw.removeWhere((element) {
-          return element.cached;
-        });
+        _pathesToDraw.removeWhere((element) => element.cached);
       });
     }).catchError((error) {
-      print("Error $error");
+      print("Update cache error $error");
     });
   }
 
@@ -140,7 +171,7 @@ class PaintWidgetState extends State<PaintWidget> {
           }
         },
         child: CustomPaint(
-          painter: _CustomPainter(_pathesToDraw, _cachedBuffer),
+          painter: _CustomPainter(_pathesToDraw, _cacheBuffer),
           size: Size.infinite,
           isComplex: true,
         ));
@@ -157,15 +188,13 @@ class _CustomPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    print("cache = $_cacheBuffer");
     if (_cacheBuffer == null) {
       canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
           new Paint()..color = Colors.white);
     } else {
-      canvas.drawImage(
-          _cacheBuffer, Offset(0, 0), Paint());
+      canvas.drawImage(_cacheBuffer, Offset(0, 0), Paint());
     }
-
-    print("size = ${_pathesToDraw.length}");
 
     for (var path in _pathesToDraw) {
       canvas.drawPath(
